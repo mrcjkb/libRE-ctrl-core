ec0552302b74b24ec160e4639b6dc94ecf374993
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-02-16 21:09:13 +0100

    #1 Replace Lib.hs with Domain module.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import LibRECtrl.Core.Domain.Power
 
 main :: IO ()
 main = someFunc
--- src/LibRECtrl/Core/Domain/Power.hs
+++ src/LibRECtrl/Core/Domain/Power.hs
@@ -1,4 +1,4 @@
-module Lib
+module LibRECtrl.Core.Domain.Power
     ( someFunc
     ) where
 
