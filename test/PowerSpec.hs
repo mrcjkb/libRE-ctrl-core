57ec410d34688c34589415d266bcf97da98436b7
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-02-28 20:46:21 +0100

    Add some PowerUnit property tests.

--- test/PowerSpec.hs
+++ test/PowerSpec.hs
@@ -16,6 +16,8 @@ tests = [
         testGroup "PowerUnitTestGroup" [
                 testProperty "SI unit should always be W." siUnitAlwaysWProperty
               , testProperty "SI offset should always be 0." offsetAlwaysZeroProperty
+              , testProperty "SI factor should alays be greater than or equal to 1 for non-user-defined PowerUnits." factorGreaterThanOrEqualToOneProperty
+              , testProperty "Ordering should be based on SI factor." orderBasedOnSiFactorProperty
            ]
       ]
 
@@ -24,6 +26,19 @@ siUnitAlwaysWProperty x = si x == W
 
 offsetAlwaysZeroProperty :: PowerUnit -> Bool
 offsetAlwaysZeroProperty x = siOffset x == 0  
+
+factorGreaterThanOrEqualToOneProperty :: PowerUnit -> Bool
+factorGreaterThanOrEqualToOneProperty UserDefinedPowerUnit {} = True
+factorGreaterThanOrEqualToOneProperty x = siFactor x >= 1
+
+orderBasedOnSiFactorProperty :: PowerUnit -> Bool
+orderBasedOnSiFactorProperty x
+                        | siFactor x > siFactor GW = x > GW
+                        | siFactor x > siFactor MW = x > MW
+                        | siFactor x > siFactor KW = x > KW
+                        | siFactor x > 1 = x > W
+                        | siFactor x == 1 = x == W
+                        | otherwise = x < W
 
 instance Arbitrary PowerUnit where
   arbitrary = createArbitraryPowerUnit
@@ -37,7 +52,7 @@ createPowerUnit W' = W
 createPowerUnit KW' = KW
 createPowerUnit MW' = MW
 createPowerUnit GW' = GW
-createPowerUnit UserDefined' = error "Cannot create user defined unit from enum."
+createPowerUnit UserDefined' = error "Cannot create user defined unit from PowerUnitBoundedEnum."
 
 -- | Helper action for creating an arbitrary power unit.
 createArbitraryPowerUnit :: Gen PowerUnit
