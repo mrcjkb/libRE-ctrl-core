488d74e0047e0bdc51db0cef78dc82100080fe5b
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-05-12 13:50:41 +0200

    Add Event module to Domain.

---
+++ src/LibRECtrl/Core/Domain/Event.hs
@@ -0,0 +1,23 @@
+module LibRECtrl.Core.Domain.Event
+  ( TimeStamped,
+    UTCTimeStampedEvent,
+  )
+where
+
+import Data.Time.Clock
+
+data Event a
+
+-- | A value that a time stamp
+class TimeStamped a where
+  -- | Returns the difference in time between two TimeStamped instances
+  diffTime :: a -> a -> NominalDiffTime
+
+-- | An event with a UTC time stamp
+data UTCTimeStampedEvent event = UTCTimeStampedEvent UTCTime event deriving (Eq, Show)
+
+instance (TimeStamped a) => TimeStamped (UTCTimeStampedEvent a) where
+  diffTime (UTCTimeStampedEvent t1 _) (UTCTimeStampedEvent t2 _) = diffUTCTime t1 t2
+
+instance (Ord a) => Ord (UTCTimeStampedEvent a) where
+  compare (UTCTimeStampedEvent t1 _) (UTCTimeStampedEvent t2 _) = compare t1 t2
